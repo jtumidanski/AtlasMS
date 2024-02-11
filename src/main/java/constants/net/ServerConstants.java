1c28ca2a1eb9c76aafa1db1aaefc039f883f519a
Chronicle20 <a.chronicle.20@gmail.com>
2024-02-11 14:47:54 -0500

    Advertise v87 version.

--- src/main/java/constants/net/ServerConstants.java
+++ src/main/java/constants/net/ServerConstants.java
@@ -3,7 +3,7 @@ package constants.net;
 public class ServerConstants {
 
     //Server Version
-    public static short VERSION = 83;
+    public static short VERSION = 87;
     //Debug Variables
     public static int[] DEBUG_VALUES = new int[10];             // Field designed for packet testing purposes
 }
